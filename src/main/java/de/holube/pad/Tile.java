134acaeab9c0d4c61e8af7943fae81814ff015c5
Tamlin <tilman@holube.de>
2023-01-25 11:44:31 +0100

    rotation

--- src/main/java/de/holube/pad/Tile.java
+++ src/main/java/de/holube/pad/Tile.java
@@ -12,22 +12,40 @@ public class Tile {
 
     private final List<int[][]> allPositions;
 
-    public Tile(int[][] base) {
+    public Tile(int[][] base, Board board) {
         this.base = base;
         baseRotated = getAllRotations(base);
-        allPositions = getAllPositions(baseRotated);
+        allPositions = getAllPositions(baseRotated, board);
+    }
+
+    private static int[][] rotate90Clockwise(int[][] origin) {
+        int[][] result = new int[origin[0].length][origin.length];
+
+        for (int i = 0; i < origin.length; i++) {
+            for (int j = 0; j < origin[i].length; j++) {
+                int newJ = origin[i].length - j;
+                result[newJ][i] = origin[i][j];
+            }
+        }
+
+        return result;
     }
 
     private List<int[][]> getAllRotations(int[][] base) {
         Set<int[][]> results = new HashSet<>();
 
         results.add(base);
-
+        int[][] tmp = rotate90Clockwise(base);
+        results.add(tmp);
+        tmp = rotate90Clockwise(tmp);
+        results.add(tmp);
+        tmp = rotate90Clockwise(tmp);
+        results.add(tmp);
 
         return results.stream().toList();
     }
 
-    private List<int[][]> getAllPositions(List<int[][]> baseRotated) {
+    private List<int[][]> getAllPositions(List<int[][]> baseRotated, Board board) {
         Set<int[][]> results = new HashSet<>();
 
 
