ee8af08e50c789578ab9f0ca4564df1f8e347767
Tamlin <tilman@holube.de>
2023-10-08 13:38:02 +0200

    allow duplicate tiles

--- src/main/java/de/holube/pad/util/Config.java
+++ src/main/java/de/holube/pad/util/Config.java
@@ -5,36 +5,37 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Config {
 
-    @Getter
-    @Setter
-    private String jsonSource;
     private final String activeBoard;
     private final Map<String, BoardConfig> boards;
     private final List<String> activeTiles;
     private final Map<String, int[][]> tiles;
-
     @Getter
     private final boolean saveSolutions;
     @Getter
     private final boolean saveImages;
     @Getter
     private final int parallelism;
+    @Getter
+    @Setter
+    private String jsonSource;
 
     public BoardConfig getBoard() {
         return boards.get(activeBoard);
     }
 
     public List<int[][]> getTiles() {
-     return tiles.entrySet().stream()
-        .filter(e -> activeTiles.contains(e.getKey()))
-        .map(Map.Entry::getValue)
-        .toList();
+        List<int[][]> arrays = new ArrayList<>(activeTiles.size());
+        for (String activeTileKey : activeTiles) {
+            arrays.add(tiles.get(activeTileKey));
+        }
+        return arrays;
     }
 
     @Getter
